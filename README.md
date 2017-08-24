5ea34e54a7234daae1c6fb61ba5ca5e7f127e937
Erika Varis Doggett <erika.varis.doggett@disney.com>
2017-08-24 08:41:35 -0700

    small changes?

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Playing with a seq2seq style chatbot trained on Early Modern English (ie., a Sha
 ## System Notes
 
 Python 3.5
+
+## About the project
+
+I put this project together mostly as a training exercise for myself, to go through the procedure of creating a simple chatbot, and also to test out the simple hypothesis that if you train a system with stylized language, the results will be in that style as well. Sounds obvious, but stylized language generation has other applications, so it's nice to confirm that it works.
+
+
--- data_prep.py
+++ data_prep.py
@@ -4,6 +4,10 @@ import xmltodict
 import collections
 import glob
 from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn
+import json
 
 NOT_LOWERCASE = re.compile(r'^[^a-z]+$')
 
@@ -15,11 +19,11 @@ def get_pairs(data):
             pairs.append(pair)
     return pairs
 
-def read_shakespeare(filename):
-    '''norvig.com/ngrams/shakespeare.txt
+def read_shakespeare():
+    '''data/shakespeare.txt
     '''
     data = []
-    with open(filename, 'r') as f:
+    with open('data/shakespeare.txt', 'r') as f:
         data = f.read()
 
     #divide by double newlines
@@ -51,6 +55,64 @@ def read_ced_xml(foldername):
         data.extend(get_pairs(dialogue))
 
     return data
+
+def read_ced_drama(filename):
+    dialogue = []
+    INTRO_TEXT = re.compile(r'\<.+\>', re.I)
+    NOTES_2 = re.compile(r'\[\^[^\^\]\[]+\^\]', re.I) #excludes ^
+    NOTES_1 = re.compile(r'\[\^[\w\s"\(\)\^\.\,\d:-]+\^\]', re.I) #covers ^
+
+    STAGE_DIR = r'\[\$[^$\]]+\$\]'
+    ANOTHER_STAGE_DIR = r'\[\}[^\}]+\}\]'
+
+    FONT = re.compile(r'\(\^[\w \']+\^\)', re.I)
+    ANOTHER_FONT = re.compile(r'\(\^ \(\\[^\^\)]+\\\) \^\)')
+    PRE_FONT = re.compile(r'\(\^(?=[\w \']+\^\))', re.I)
+    POST_FONT = re.compile(r'(?<=[a-zA-Z])\^\)', re.I)
+    PRE_ANOTHER_FONT = re.compile(r'\(\^ \(\\(?=[^\\\)]+\\\) \^\))')
+    POST_ANOTHER_FONT = re.compile(r'(?<=[a-zA-Z.])\\\) \^\)')
+
+    DIALOGUE_SPLIT = re.compile(STAGE_DIR + '|' + ANOTHER_STAGE_DIR)
+
+
+    with open(filename, 'rb') as f:
+        data = f.read()
+        try:
+            data = data.decode('utf-8')
+        except UnicodeDecodeError: #some files contain unidified encoding
+            data = ''.join(map(chr, data)) #manual conversion of bytes to str
+    #take out non-dialogue texts
+    data = re.sub(INTRO_TEXT, '', data)
+    data = re.sub(NOTES_2, '', data)
+    if re.findall(NOTES_1, data): 
+        data = re.sub(NOTES_1, '', data)
+    data = re.sub('#\n', '\n', data) #some texts have # before line breaks
+    
+    if re.findall(ANOTHER_FONT, data): #take out weird font notations around words
+        data = re.sub(PRE_ANOTHER_FONT, '', data)
+        data = re.sub(POST_ANOTHER_FONT, '', data)
+
+    if re.findall(FONT, data): #smaller version of weird font notation
+        data = re.sub(PRE_FONT, '', data)
+        data = re.sub(POST_FONT, '', data)
+
+    data = re.sub('   ', '', data)
+
+    dialogue = re.split(DIALOGUE_SPLIT, data)
+    clean_dialogue = []
+    for i,line in enumerate(dialogue):
+        line = line.rstrip()
+        #line = line.rstrip('\\n')
+        if line == '.':
+            continue
+        if line == '\n':
+            continue
+        if line == '':
+            continue
+        else:
+            clean_dialogue.append(line)
+
+    return clean_dialogue
 
 def read_ced_txt(filename):
     #This will cover both trials and didactic works
@@ -115,12 +177,15 @@ def read_ced():
     '''Open up each CED file relevant to our dialogue test
     and extract the dialogues from them.
     '''
-    foldername = '2507/2507/CEDPlain'
+    foldername = 'data/2507/2507/CEDPlain' #adjust to wherever CED data lives
     dialogue_pairs = []
     #comedy dramas
     for filename in glob.glob(foldername+'/D?C*'):
         print(filename)
         dialogue = read_ced_txt(filename)
+        #error message
+        if len(dialogue) < 20:
+            print('Potential Error in parsing:', filename)
         pairs = get_pairs(dialogue)
         dialogue_pairs.extend(pairs)
 
@@ -128,37 +193,87 @@ def read_ced():
     for filename in glob.glob(foldername+'/D?T*'):
         print(filename)
         dialogue = read_ced_txt(filename)
+        #error message
+        if len(dialogue) < 20:
+            print('Potential Error in parsing:', filename)
         pairs = get_pairs(dialogue)
         dialogue_pairs.extend(pairs)
 
     #didactics
     for filename in glob.glob(foldername+'/D?H*'):
-        print(filename)
-        dialogue = read_ced_txt(filename)
-        pairs = get_pairs(dialogue)
-        dialogue_pairs.extend(pairs)
+        #don't do D1HFDESA; it has no character separations
+        if not filename.endswith('D1HFDESA'):
+            print(filename)
+            dialogue = read_ced_txt(filename)
+            #error message
+            if len(dialogue) < 20:
+                print('Potential Error in parsing:', filename)
+            pairs = get_pairs(dialogue)
+            dialogue_pairs.extend(pairs)
 
     #miscellaneous
     for filename in glob.glob(foldername + '/D?M*'):
-        print(filename)
-        dialogue = read_ced_txt(filename)
-        pairs = get_pairs(dialogue)
-        dialogue_pairs.extend(pairs)
+        #don't do D3HFMAUG, it's not fixed yet
+        if not filename.endswith('D3HFMAUG'):
+            print(filename)
+            dialogue = read_ced_txt(filename)
+            #error message
+            if len(dialogue) < 20:
+                print('Potential Error in parsing:', filename)
+            pairs = get_pairs(dialogue)
+            dialogue_pairs.extend(pairs)
 
     return dialogue_pairs
+
+def write_datafiles(dialogue_pairs):
+    with open('data/input_data.json', 'w') as wi:
+        with open('data/output_data.json', 'w') as wo:
+            for pair in dialogue_pairs:
+                itext = {'text': pair[0]}
+                otext = {'text': pair[1]}
+                json.dump(itext, wi)
+                wi.write('\n')
+                json.dump(otext, wo)
+                wo.write('\n')
 
 #quick wordcount
 def wordcount(dialogue_pairs):
     words = set()
     words_l = []
+    lengths = []
     for pair in dialogue_pairs:
-        for utterance in pair:
+        l = {}
+        for i, utterance in enumerate(pair):
             tokens = word_tokenize(utterance)
+            l['l'+str(i)] = len(tokens)
             words.update(tokens)
             words_l.extend(tokens)
+        lengths.append(l)
 
     print('unique words:', len(words))
     print('total word count:', len(words_l))
+
+    return lengths
+
+def bucket_description(lengths):
+    
+    df = pd.DataFrame(lengths)
+    #column for sequence of l0 and l1
+    def f(row):
+        return [row['l0'], row['l1']]
+
+    df['sequence'] = df.apply(f, axis=1)
+    df['difference'] = df['l1'] - df['l0']
+    #histogram & descriptive stats to determine range of most data
+    df['difference'].describe()
+    df['difference'].plot.hist()
+    #take slice of most frequent data dist
+    middle_slice = df[(df['difference']>= -14) & (df['difference']>= 14)]
+    #cross tabs to see combinations of l0 and l1
+    crosstabs = pd.crosstab(middle_slice['l0'], middle_slice['l1'])
+    #plot crosstabs in heatmap
+    f,x = plt.subplots()
+    seaborn.heatmap(crosstabs)
 
 
 
